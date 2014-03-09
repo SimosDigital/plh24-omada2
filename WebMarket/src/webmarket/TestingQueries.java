8bb126fda7a546eb434ad340418f5493a990aeb8
SimosDigital <simosdigital@gmail.com>
2014-03-09 21:19:35 +0000

    Λίγα πράγματα στη συσχέτηση προϊόντων - καταστημάτων

--- WebMarket/src/webmarket/TestingQueries.java
+++ WebMarket/src/webmarket/TestingQueries.java
@@ -19,6 +19,10 @@ public class TestingQueries {
     private static EntityManager em;
     private static javax.persistence.Query query1;
     private static javax.persistence.Query query2;
+    private static javax.persistence.Query storeProductQuery;
+    static List<Product> storeProductList;
+    
+    
     
    
     public static void main(String args[]) {
@@ -28,20 +32,20 @@ public class TestingQueries {
         query1 = em.createQuery("SELECT s FROM Store s WHERE s.storeId=:storeId");
         query1.setParameter("storeId", 3);
         List<Store> selectedStore = query1.getResultList();
-        
-        
         Store selStore = selectedStore.get(0);
         
+        storeProductQuery = em.createQuery("SELECT s.productId FROM StoreProduct s WHERE s.storeId=:storeId").setParameter("storeId", selectedStore.get(0));
+        storeProductList = storeProductQuery.getResultList();
       
-        query2 = em.createQuery("SELECT s.productId FROM StoreProduct s WHERE s.storeId=:storeId").setParameter("storeId", selStore);
 //      query2 = em.createQuery("SELECT s FROM StoreProduct s JOIN Product p ON s.productId = p.productId WHERE s.storeId=:storeId");
 //      query2 = em.createQuery("SELECT p FROM Product p JOIN s FROM StoreProduct s WHERE Store.Product s WHERE p.productId=:p.productId AND s.storeId=:storeId\");+ \" pproductId=selprods.productId");                
 //      query2 = em.createQuery("SELECT p FROM Product p JOIN selprods WHERE pproductId=selprods.productId");        
 //      query2 = em.createQuery("SELECT p FROM Product p JOIN StoreProduct s ON p.productId=s.productId WHERE s.storeId=:storeId");
+//      query2.setParameter("storeId", selStore);
 
-//     query2.setParameter("storeId", selStore);
         
-        List<Product> selProduct = query2.getResultList();
+   query2 = em.createQuery("SELECT s.productId FROM StoreProduct s WHERE s.storeId=:storeId").setParameter("storeId", selStore);
+      List<Product> selProduct = query2.getResultList();
         
         
  
@@ -54,10 +58,17 @@ public class TestingQueries {
 //        Store tempStore = stlist.get(0);
 
         System.out.println("\nΚατάστημα : " + selStore.getName() + ".\nΑρ. προϊόντων : " + selProduct.size()+ "\n");
+        
         for (Product pr : selProduct) {
             System.out.println(pr.getName());
         }
+        
+        System.out.println("\nΚατάστημα : " + selStore.getName() + ".\nΑρ. προϊόντων : " + selProduct.size()+ "\n");
+        
                 
+        for (Product pr : storeProductList) {
+            System.out.println(pr.getName());
+        }
         
 
 
