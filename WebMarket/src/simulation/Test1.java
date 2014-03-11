c9d246cf7a4eba9a08e2b9948cdd6155e07b73e3
triasam <triasam@gmail.com>
2014-03-11 15:04:25 +0000

    neotero

--- WebMarket/src/simulation/Test1.java
+++ WebMarket/src/simulation/Test1.java
@@ -46,29 +46,53 @@ public class Test1 {
 // Διαβάζουμε τους πελάτες
         customersList = customersQuery.getResultList();
         int cCount    = customersList.size();
-        randCustomer = new Random();
+        randCustomer  = new Random();
 // Διαβάζουμε τα καταστηματα
         storesList = storesQuery.getResultList();
         int sCount = customersList.size();
-        int loopCustomers = cCount;
+        randStore  = new Random();
+        List<ProductPurchase> ppList = null ;
+	Integer pointsEarned = 0;
+	Float   amount       = 0F;
 // Κάνουμε τόσες αγορές όσοι και οι πελάτες
+        int loopCustomers = cCount;
         while (loopCustomers > 0) {
           value = randCustomer.nextInt(cCount);      
           Customer c = customersList.get(value);
-       System.out.println(c);
+          System.out.println(c);
 // Για κάθε αγορά διαλέγουμε στην τύχη ένα κατάστημα
-          randStore = new Random();
           value = randStore.nextInt(sCount);
           Store s = storesList.get(value);
-       System.out.println(s);
+          System.out.println(s);
 // Για κάθε κατάστημα διαβάζουμε τα προιόντα του
           storeProductsQuery.setParameter("storeId", s);
           productsList = storeProductsQuery.getResultList();
+// Δημιουργούμε μια παραγγελία με όλα τα προιόντα
+// που λειτουργεί σαν καλάθι                     
+          Purchase        pr = new Purchase();
+          ProductPurchase pp = new ProductPurchase();
+          ppList       = null ;
+	  pointsEarned = 0;
+	  amount       = 0F;
           for (Product p : productsList) {
              System.out.println(p);
+             pp = new ProductPurchase();
+    	     pp.setProductId(p);
+	     pp.setPurchaseId(pr);
+	     pp.setQuantity(1);
+             ppList.add(pp);
+	     pointsEarned += p.getPoints();
+	     amount       += p.getPrice();
           }
+    	  pr.setCustomerId(c);
+    	  pr.setStoreId(s);
+    	  pr.setDelivery(0);
+	  pr.setPointsEarned(pointsEarned);
+	  pr.setAmount(amount);
+          System.out.println(pr);
           loopCustomers -= 1;
         }
 
     }
 }
+
